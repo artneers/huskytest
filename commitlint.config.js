1218072be4d7d43f7e8f9a5e1e96cd592fe3ccc9
Owen <502021767@qq.com>
2022-04-01 16:28:36 +0800

    initial the  project

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "docs",
+        "chore",
+        "feat",
+        "fix",
+        "merge",
+        "improvement",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "ci",
+        "build"
+      ]
+    ]
+  }
+}
